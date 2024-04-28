b5e5277c2dbe7ba7dc8da81e6fc3e641b3b12003
adnan <adnanhabib152572@gmail.com>
2024-04-29 01:30:16 +0600

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,27 @@ import Header from './components/Header/Header'
 
 function App() {
    const [bookmarks, setbookmarks] = useState([]);
+   const [readingtime, setreadingtime] = useState(0);
 
-   const handleAddToBookmark = blog =>{
-    console.log('adding soon');
+   const handleAddToBookmark =  blog =>{
+    const newBookmarks = [...bookmarks, blog]
+    setbookmarks(newBookmarks);
+   }
+   const handlemarkasread =(id, time)=>{
+    const newreadingtime =readingtime + time;
+    setreadingtime( newreadingtime);
+
+    const remainingBookmarks = bookmarks.filter(bookmark=>bookmark.id!==id);
+    setbookmarks(remainingBookmarks);
+     
    }
 
   return (
     <>
       <Header ></Header> 
       <div className='md:flex max-w-7xl mx-auto'>
-        <Blogs handleAddToBookmark={handleAddToBookmark}></Blogs> 
-        <Bookmarks></Bookmarks>
+        <Blogs handleAddToBookmark={handleAddToBookmark} handlemarkasread={handlemarkasread}></Blogs> 
+        <Bookmarks bookmarks={bookmarks} readingtime={readingtime} ></Bookmarks>
       </div>  
     </>
   )
